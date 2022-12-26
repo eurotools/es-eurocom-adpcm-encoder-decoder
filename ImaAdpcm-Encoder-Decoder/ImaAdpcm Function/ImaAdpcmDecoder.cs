00a2cd7ddc325c4b3c26d9087dfed196bdf309f1
jmarti856 <jmarti856@gmail.com>
2022-12-26 21:58:05 +0100

    Initial commit

---
+++ ImaAdpcm-Encoder-Decoder/ImaAdpcm Function/ImaAdpcmDecoder.cs
@@ -0,0 +1,106 @@
+﻿namespace ImaAdpcm_Tool
+{
+    //-------------------------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------------------------------------
+    public static partial class ImaCodec
+    {
+        //-------------------------------------------------------------------------------------------------------------------------------
+        public static short[] Decode(byte[] adpcmData)
+        {
+            int numSamples = adpcmData.Length * 56 / 32;
+            short[] outBuff = new short[numSamples];
+            int inp;           		/* Input buffer pointer */
+            int outIndex = 0;   	/* Output buffer pointer */
+            int sign;           	/* Current adpcm sign bit */
+            int delta;          	/* Current adpcm output value */
+            int step;           	/* Stepsize */
+            int valpred;        	/* Predicted value */
+            int vpdiff;         	/* Current change to valpred */
+            int index;          	/* Current step change index */
+            int inputbuffer;        /* Place to keep next 4-bit value */
+            int remainingSamples;   /* Countdown remaining samples for the output buffer */
+            bool bufferstep;		/* Toggle between inputbuffer/input */
+
+            ImaAdpcmState state = new ImaAdpcmState();
+            inp = 0;
+            inputbuffer = 0;
+
+            valpred = state.valprev;
+            index = state.index;
+            step = stepsizeTable[index];
+            remainingSamples = 0;
+
+            bufferstep = false;
+
+            for (; numSamples > 0; numSamples--)
+            {
+                if (--remainingSamples <= 0)
+                {
+                    remainingSamples = 56;
+                    valpred = (short)(adpcmData[inp++] | (sbyte)adpcmData[inp++] << 8);
+                    index = adpcmData[inp]; inp += 2;
+
+                    if (index > 88)
+                    {
+                        index = 0;
+                    }
+                }
+
+                /* Step 1 - get the delta value */
+                if (bufferstep)
+                {
+                    delta = inputbuffer & 0xf;
+                }
+                else
+                {
+                    inputbuffer = adpcmData[inp++];
+                    delta = (inputbuffer >> 4) & 0xf;
+                }
+                bufferstep = !bufferstep;
+
+                /* Step 2 - Find new index value (for later) */
+                index += indexTable[delta];
+                if (index < 0) index = 0;
+                if (index > 88) index = 88;
+
+                /* Step 3 - Separate sign and magnitude */
+                sign = delta & 8;
+                delta &= 7;
+
+                /* Step 4 - Compute difference and new predicted value */
+                /*
+                ** Computes 'vpdiff = (delta+0.5)*step/4', but see comment
+                ** in adpcm_coder.
+                */
+                vpdiff = step >> 3;
+                if ((delta & 4) != 0) vpdiff += step;
+                if ((delta & 2) != 0) vpdiff += step >> 1;
+                if ((delta & 1) != 0) vpdiff += step >> 2;
+
+                if (sign != 0)
+                    valpred -= vpdiff;
+                else
+                    valpred += vpdiff;
+
+                /* Step 5 - clamp output value */
+                if (valpred > short.MaxValue)
+                    valpred = short.MaxValue;
+                else if (valpred < short.MinValue)
+                    valpred = short.MinValue;
+
+                /* Step 6 - Update step value */
+                step = stepsizeTable[index];
+
+                /* Step 7 - Output value */
+                outBuff[outIndex++] = (short)valpred;
+            }
+            state.valprev = valpred;
+            state.index = index;
+
+            return outBuff;
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------
+}
---
+++ ImaAdpcm-Encoder-Decoder/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ImaAdpcm_Tool
+{
+    //-------------------------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------------------------------------
+    //-------------------------------------------------------------------------------------------------------------------------------
+    class Program
+    {
+        //-------------------------------------------------------------------------------------------------------------------------------
+        private static void Main(string[] args)
+        {
+            //Ensure that we have arguments
+            if (args.Length > 0)
+            {
+                //Show help if required
+                if (args.Length == 1)
+                {
+                    if (args[0].Equals("help", StringComparison.OrdinalIgnoreCase) || args[0].Equals("?"))
+                    {
+                        Console.WriteLine("Ima Adpcm Tool. By jmarti856");
+                        Console.WriteLine("Info: Supports 16 bit WAV files.");
+                        Console.WriteLine("");
+                        Console.WriteLine("------------------------------------For Encoding------------------------------------");
+                        Console.WriteLine("Usage: <InputFile> <OutputFile>");
+                        Console.WriteLine("");
+                        Console.WriteLine("------------------------------------For Decoding------------------------------------");
+                        Console.WriteLine("Usage: Decode <InputFile> <OutputFile> <frequency>");
+                    }
+                }
+                else
+                {
+                    if (args.Length > 1)
+                    {
+                        string inputFile = args[0];
+                        //Check if we have to execute the decoder
+                        if (inputFile.Equals("Decode", StringComparison.OrdinalIgnoreCase))
+                        {
+                            if (args.Length > 3)
+                            {
+                                inputFile = args[1];
+                                if (ProgramFunctions.CheckFileExists(inputFile))
+                                {
+                                    ProgramFunctions.ExecuteDecoder(inputFile, args[2].Trim(), Convert.ToInt32(args[3].Trim()));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            //Execute encoder
+                            if (ProgramFunctions.CheckFileExists(inputFile))
+                            {
+                                ProgramFunctions.ExecuteEncoder(inputFile, args[1].Trim());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //-------------------------------------------------------------------------------------------------------------------------------
+}
